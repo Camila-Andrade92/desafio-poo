4fdc351640f9ab8df7debd7834dbf0723ac9deb5
Camila Andrade Gomes <gomescandrade@gmail.com>
2022-03-31 20:49:51 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descrição do curso java");
+
+        Curso curso2 = new Curso();
+        curso2.setCargaHoraria(8);curso1.setTitulo("Curso Js");
+        curso2.setDescricao("descrição do curso js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("descrição java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+    }
+}
