b3b71753eed3bf74b43d22716804e4f47c2e99ff
Camila Andrade Gomes <gomescandrade@gmail.com>
2022-04-03 18:42:04 -0300

    Inserção da lista dos 3 melhores devs do bootcamp

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
+import jdk.swing.interop.SwingInterOpUtils;
 
 import java.time.LocalDate;
 
@@ -7,21 +10,145 @@ public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
         curso1.setTitulo("Curso Java");
-        curso1.setDescricao("descrição do curso java");
+        curso1.setDescricao("Descrição do Curso Java");
+        curso1.setCargaHoraria(6);
 
         Curso curso2 = new Curso();
-        curso2.setCargaHoraria(8);curso1.setTitulo("Curso Js");
-        curso2.setDescricao("descrição do curso js");
+        curso2.setTitulo("Curso Introdução a Testes de Software");
+        curso2.setDescricao("Descrição Curso Introdução a Testes de Software");
         curso2.setCargaHoraria(4);
+
+        Curso curso3 = new Curso();
+        curso3.setTitulo("Curso Testes Unitários com jUnit");
+        curso3.setDescricao("Descrição Curso Testes Unitários com jUnit");
+        curso3.setCargaHoraria(2);
+
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria java");
-        mentoria.setDescricao("descrição java");
+        mentoria.setDescricao("Descrição Mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+
+
+        Bootcamp bootcamp = new Bootcamp();
+           bootcamp.setNome("GFT Quality Assurance");
+           bootcamp.setDescricao("Descrição Bootcamp GFT Quality Assurance");
+           bootcamp.getConteudos().add(curso1);
+           bootcamp.getConteudos().add(curso2);
+           bootcamp.getConteudos().add(curso3);
+           bootcamp.getConteudos().add(mentoria);
+
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setNome("Java GFT");
+        bootcamp1.setDescricao("Descrição Bootcamp Java GFT");
+        bootcamp1.getConteudos().add(curso1);
+        bootcamp1.getConteudos().add(curso2);
+        bootcamp1.getConteudos().add(curso3);
+        bootcamp1.getConteudos().add(mentoria);
+
+
+        System.out.println("****************************************************************************************");
+
+        System.out.println();
+        Dev devCamila = new Dev();
+        devCamila.setNome("CAMILA ANDRADE");
+        System.out.println("Aluno inscrito: "+devCamila.getNome());
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Bootcamp inscrito: " +bootcamp.getNome());
+        System.out.println("Conteudos inscritos: \n" + devCamila.getConteudosInscritos()+" ");
+        devCamila.progredir();
+        devCamila.progredir();
+        devCamila.progredir();
+        devCamila.progredir();
+        System.out.println("**********-----**********");
+        System.out.println("Conteudos inscritos após progressão: \n" + devCamila.getConteudosInscritos());
+        System.out.println("**********-----**********");
+        System.out.println("Conteudos concluídos: \n" + devCamila.getConteudosConcluidos());
+        System.out.println("Pontuação XP: "+devCamila.calcularTotalXp());
+        System.out.println("**********-----**********");
+        devCamila.aprovado(bootcamp);
+        if(devCamila.aprovado(bootcamp)){
+            System.out.println("Você concluiu o BootCamp, boa sorte nas próximas etapas!!");
+        }else {
+            System.out.println("Não foi dessa vez, você não foi aprovado no bootcamp, mas não desista, teremos outras oportunidade para você.");
+        }
+
+
+        System.out.println("------------------------------------------------------------------------------------------------");
+
+        System.out.println();
+        Dev devLucio = new Dev();
+        devLucio.setNome("LÚCIO RIBEIRO");
+        System.out.println("Aluno inscrito: "+devLucio.getNome());
+        devLucio.inscreverBootcamp(bootcamp);
+        System.out.println("Bootcamp inscrito: " +bootcamp.getNome());
+        System.out.println("Conteudos inscritos: \n" + devLucio.getConteudosInscritos()+" ");
+        devLucio.progredir();
+        devLucio.progredir();
+        System.out.println("**********-----**********");
+        System.out.println("Conteudos inscritos após progressão: \n" + devLucio.getConteudosInscritos());
+        System.out.println("**********-----**********");
+        System.out.println("Conteudos concluídos: \n" + devLucio.getConteudosConcluidos());
+        System.out.println("Pontuação XP: "+devLucio.calcularTotalXp());
+        System.out.println("**********-----**********");
+        devLucio.aprovado(bootcamp);
+        if(devLucio.aprovado(bootcamp)){
+            System.out.println("Você concluiu o BootCamp, boa sorte nas próximas etapas!!");
+        }else {
+            System.out.println("Não foi dessa vez, você não foi aprovado no bootcamp, mas não desista, teremos outras oportunidade para você.");
+        }
+
+        System.out.println();
+        Dev devMaria = new Dev();
+        devMaria.setNome("MARIA RIBEIRO");
+        System.out.println("Aluno inscrito: "+devMaria.getNome());
+        devMaria.inscreverBootcamp(bootcamp);
+        System.out.println("Bootcamp inscrito: " +bootcamp.getNome());
+        System.out.println("Conteudos inscritos: \n" + devMaria.getConteudosInscritos()+" ");
+        devMaria.progredir();
+        devMaria.progredir();
+        devMaria.progredir();
+        System.out.println("**********-----**********");
+        System.out.println("Conteudos inscritos após progressão: \n" + devMaria.getConteudosInscritos());
+        System.out.println("**********-----**********");
+        System.out.println("Conteudos concluídos: \n" + devMaria.getConteudosConcluidos());
+        System.out.println("Pontuação XP: "+devMaria.calcularTotalXp());
+        System.out.println("**********-----**********");
+        devMaria.aprovado(bootcamp);
+        if(devMaria.aprovado(bootcamp)){
+            System.out.println("Você concluiu o BootCamp, boa sorte nas próximas etapas!!");
+        }else {
+            System.out.println("Não foi dessa vez, você não foi aprovado no bootcamp, mas não desista, teremos outras oportunidade para você.");
+        }
+
+        System.out.println();
+        Dev devMiguel = new Dev();
+        devMiguel.setNome("MIGUEL RIBEIRO");
+        System.out.println("Aluno inscrito: "+devMiguel.getNome());
+        devMiguel.inscreverBootcamp(bootcamp);
+        System.out.println("Bootcamp inscrito: " +bootcamp.getNome());
+        System.out.println("Conteudos inscritos: \n" + devMiguel.getConteudosInscritos()+" ");
+        devMiguel.progredir();
+        devMiguel.progredir();
+        System.out.println("**********-----**********");
+        System.out.println("Conteudos inscritos após progressão: \n" + devMiguel.getConteudosInscritos());
+        System.out.println("**********-----**********");
+        System.out.println("Conteudos concluídos: \n" + devMiguel.getConteudosConcluidos());
+        System.out.println("Pontuação XP: "+devMiguel.calcularTotalXp());
+        System.out.println("**********-----**********");
+        devMiguel.aprovado(bootcamp);
+        if(devMiguel.aprovado(bootcamp)){
+            System.out.println("Você concluiu o BootCamp, boa sorte nas próximas etapas!!");
+        }else {
+            System.out.println("Não foi dessa vez, você não foi aprovado no bootcamp, mas não desista, teremos outras oportunidade para você.");
+        }
+
+        System.out.println("Bootcamp: " + bootcamp.getNome());
+        System.out.println("Os melhores desenvolvedores:");
+        for( Dev dev : bootcamp.tresMelhoresDevs()){
+            System.out.println(dev.getNome());
+        }
 
 
     }
---
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,116 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class Bootcamp {
+    private String nome;
+    private String descricao;
+    private LocalDate dataInicial = LocalDate.now();
+    private final LocalDate dataFinal = dataInicial.plusDays(45);
+    private Set<Dev> devsInscritos = new HashSet<>();
+    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
+
+   public ArrayList<Dev> tresMelhoresDevs() {
+
+       ArrayList<Dev> todosDevs = new ArrayList<>();
+       todosDevs.addAll(this.devsInscritos);
+       ArrayList<Dev> melhoresDevs = new ArrayList<>();
+
+       if(this.devsInscritos.size()>=3) {
+           double melhorxp = todosDevs.get(0).calcularTotalXp();
+           Dev melhorDev = todosDevs.get(0);
+
+           for (Dev dev : todosDevs) {
+               if (dev.calcularTotalXp() > melhorxp) {
+                   melhorxp = dev.calcularTotalXp();
+                   melhorDev = dev;
+               }
+           }
+           melhoresDevs.add(melhorDev);
+           todosDevs.remove(melhorDev);
+
+           melhorxp = todosDevs.get(0).calcularTotalXp();
+           melhorDev = todosDevs.get(0);
+           for (Dev dev : todosDevs) {
+               if (dev.calcularTotalXp() > melhorxp) {
+                   melhorxp = dev.calcularTotalXp();
+                   melhorDev = dev;
+               }
+           }
+           melhoresDevs.add(melhorDev);
+           todosDevs.remove(melhorDev);
+
+           melhorxp = todosDevs.get(0).calcularTotalXp();
+           melhorDev = todosDevs.get(0);
+           for (Dev dev : todosDevs) {
+               if (dev.calcularTotalXp() > melhorxp) {
+                   melhorxp = dev.calcularTotalXp();
+                   melhorDev = dev;
+               }
+           }
+           melhoresDevs.add(melhorDev);
+       }
+
+       return melhoresDevs;
+   }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(LocalDate dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public LocalDate getDataFinal() {
+        return dataFinal;
+    }
+
+    public Set<Dev> getDevsInscritos() {
+        return devsInscritos;
+    }
+
+    public void setDevsInscritos(Set<Dev> devsInscritos) {
+        this.devsInscritos = devsInscritos;
+    }
+
+    public Set<Conteudo> getConteudos() {
+        return conteudos;
+    }
+
+    public void setConteudos(Set<Conteudo> conteudos) {
+        this.conteudos = conteudos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicial, bootcamp.dataInicial) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(devsInscritos, bootcamp.devsInscritos) && Objects.equals(conteudos, bootcamp.conteudos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, descricao, dataInicial, dataFinal, devsInscritos, conteudos);
+    }
+}
